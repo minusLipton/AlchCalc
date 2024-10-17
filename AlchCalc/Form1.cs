0b8b295594a9062ce1c304c083b3e0f7885ce83b
minusLipton <mamutantum@gmail.com>
2024-10-17 21:07:44 +0200

    temp obliczenia

--- AlchCalc/Form1.cs
+++ AlchCalc/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace AlchCalc
 {
@@ -15,6 +16,28 @@ namespace AlchCalc
         public Form1()
         {
             InitializeComponent();
+
+            //temp
+            Double size = 30;
+            Double contents = 14;
+            int amount = 2;
+            //Double size = Convert.ToDouble(tbSize.Text);
+            //Double contents = Convert.ToDouble(tbContents.Text);
+            //int amount = Convert.ToInt32(tbAmount.Text);
+
+
+            double totalVolume = size * amount;
+            double volumeOfAlcohol = totalVolume * (contents / 100);
+            double volumeOfRest = totalVolume - volumeOfAlcohol;
+
+            
+            //Console.WriteLine($"Volume of alcohol: {volumeOfAlcohol} ml");
+            //Console.WriteLine($"Volume of the rest of the drink: {volumeOfRest} ml");
+
+            tbSize.Text = volumeOfAlcohol.ToString();
+            tbContents.Text = volumeOfRest.ToString();
+            
+
         }
     }
 }
